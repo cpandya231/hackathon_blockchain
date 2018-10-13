4ead739dd888227ded3bb86c2c29cc464bd1b724
nishraddha <shraddhanistane@gmail.com>
2018-10-14 02:25:06 +0530

    claim table added

--- src/js/app.js
+++ src/js/app.js
@@ -104,11 +104,18 @@ App = {
 
     }).then(function (claims) {
       console.log(claims.length);
+      var claimView = $("#claimView");
+      var trContent = $("#trContent");
+      claimView.empty();
+      trContent.show();
       for (i = 0; i < claims.length; i++) {
         //console.log("Claim id "+claims[i].c);
         var claim;
         instance.claims(claims[i].c).then(function(res){
-          claim=res
+          claim=res;
+         
+          var claimTemplate = "<tr><th><a href=" + claim[0] + ">"+ claim[0] +"</th><td>"+claim[1]+"</td><td>"+claim[2]+"</td></tr>"
+          claimView.append(claimTemplate);
           console.log("Resource Link "+claim[0]+" Amount to be spend "+ claim[1]+" Wait time "+claim[2]);
         });
         
