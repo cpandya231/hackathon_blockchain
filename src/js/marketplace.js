b97b583c09087aeab69a27222b9b78853c090053
Chintan Pandya <Chintan.Pandya@ezest.local>
2018-10-14 01:58:00 +0530

    Added marketplace

--- src/js/marketplace.js
+++ src/js/marketplace.js
@@ -4,7 +4,7 @@ App = {
   account: '0x0',
   hasVoted: false,
   balance: null,
-  returnClaims:[],
+  returnClaims: [],
 
   init: function () {
     return App.initWeb3();
@@ -64,6 +64,7 @@ App = {
       }
     });
 
+    App.findAllClaims();
 
 
 
@@ -107,19 +108,19 @@ App = {
       for (i = 0; i < claims.length; i++) {
         //console.log("Claim id "+claims[i].c);
         var claim;
-        instance.claims(claims[i].c).then(function(res){
-          claim=res
-          console.log("Resource Link "+claim[0]+" Amount to be spend "+ claim[1]+" Wait time "+claim[2]);
+        instance.claims(claims[i].c).then(function (res) {
+          claim = res
+          console.log("Resource Link " + claim[0] + " Amount to be spend " + claim[1] + " Wait time " + claim[2]);
         });
-        
+
         //returnClaims.push(claims[i].c);
       }
       console.log(returnClaims);
       //returnClaims.push(claims[4].c);
     });
-    
+
   },
-  findAllClaims: function(){
+  findAllClaims: function () {
     var claimInfo = $("#claimInfo");
     claimInfo.empty();
     returnClaims = [];
@@ -127,20 +128,20 @@ App = {
     App.contracts.Consumer.deployed().then(function (i) {
       instance = i;
       return instance.claimCount();
-    }).then(function(claimCount){
+    }).then(function (claimCount) {
       console.log(claimCount);
-      for(i=0;i<claimCount;i++){
+      for (i = 0; i < claimCount; i++) {
         var claim;
-        instance.claims(i).then(function(res){
-          claim=res
-          console.log("Resource Link "+claim[0]+" Amount to be spend "+ claim[1]+" Wait time "+claim[2]);
-          var claimTemplate = "<tr><th><a href=" + claim[0] + ">"+ claim[0] +"</th>"
-          claimInfo.append(candidateTemplate);
+        instance.claims(i).then(function (res) {
+          claim = res
+          console.log("Resource Link " + claim[0] + " Amount to be spend " + claim[1] + " Wait time " + claim[2]);
+          var claimTemplate = "<tr><th><a href=" + claim[0] + ">" + claim[0] + "</a></th><td><button class='button-success pure-button researcher'>Researcher</button></td><td><button class='button-error pure-button verifier'>Verifier</button></td><td><button onclick=window.location.href='witnessed.html' class=button-warning pure-button witnessed>Witness</button></td></tr>"
+          claimInfo.append(claimTemplate);
         });
       }
     });
   }
- 
+
 
 };
 
