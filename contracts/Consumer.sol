cf7366be42231be78ade2716c0e14d1dfd57a353
Chintan Pandya <Chintan.Pandya@ezest.local>
2018-10-13 13:14:35 +0530

    Consumer clain contract business logic

--- contracts/Consumer.sol
+++ contracts/Consumer.sol
@@ -5,22 +5,23 @@ contract Consumer {
         string resourceLink;
         int256 priceAmount;
         uint256 waitTime;
-        
+        address consumerAddress;
     }
-    Claim public claim;
-    uint256 public claimNo;
+    mapping(uint256 => Claim) public claims;
     
+    uint256 public claimCount;
     event ClaimEvent(uint256 _claimNo);
-    constructor() public {
+    constructor() public{
         
     }
     
-    function createClaim (string _resourceLink,int256 _priceAmount, uint256 _waitTime){
-        claimNo++;
-        claim.resourceLink =_resourceLink;
-        claim.priceAmount=_priceAmount;
-        claim.waitTime=_waitTime;
-        ClaimEvent(claimNo);
-        return claimNo;
+    function createClaim (string _resourceLink,int256 _priceAmount, uint256 _waitTime,address _consumerAddress) returns (uint256) {
+        claimCount++;
+        claims[claimCount].resourceLink =_resourceLink;
+        claims[claimCount].priceAmount=_priceAmount;
+        claims[claimCount].waitTime=_waitTime;
+        claims[claimCount].consumerAddress=_consumerAddress;
+        emit ClaimEvent(claimCount);
+        return claimCount;
     }
 }
